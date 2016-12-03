0624d27f8f9841efa670a614f25be13dd6b54cec
YinPC <85171642@163.com>
2016-12-04 05:12:27 +0800

    修改ProtoNum文件类型，拷贝Resource下。

--- XiaoGameClient/Assets/Scripts/Tools/MyPath.cs
+++ XiaoGameClient/Assets/Scripts/Tools/MyPath.cs
@@ -2,16 +2,21 @@
 using UnityEngine;
 namespace RedStone
 {
-	public class MyPath
-	{
-		public static string PROJECT
-		{
-			get { return Application.dataPath + "/../../"; }
-		}
+    public class MyPath
+    {
+        public static string PROJECT
+        {
+            get { return Application.dataPath + "/../../"; }
+        }
 
-		public static string RES_UI
-		{
-			get { return "Prefabs/UI/"; }
-		}
-	}
+        public static string RES_UI
+        {
+            get { return "Prefabs/UI/"; }
+        }
+
+        public static string RES_PROTO_NUM
+        {
+            get { return "Config/protoNum"; }
+        }
+    }
 }

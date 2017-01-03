e59c4af28bf90f5841da62ce28753b4b79115de8
yinlong <85171642@163.com>
2017-01-03 18:42:27 +0800

    添加项目结构

---
+++ GameServer/MainServer/MainServer/Proxy/HallProxy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using message;
+using RedStone.Net;
+
+namespace RedStone
+{
+	public class HallProxy : ProxyBase
+	{
+		private PlayerData m_mainPlayerData = new PlayerData();
+		public PlayerData mainPlayerData { get { return m_mainPlayerData; } }
+
+		public HallProxy()
+		{
+			netType = NetType.Hall;
+		}
+
+		public override void OnInit()
+		{
+			base.OnInit();
+		}
+
+		public override void OnDestroy()
+		{
+			base.OnDestroy();
+		}
+
+		public void AssignRoom()
+		{
+			AssignRoomRequest msg = new AssignRoomRequest();
+			network.SendMessage<AssignRoomRequest, AssignRoomReply>(msg,
+			(reply) =>
+			{
+				SendEvent(Event.Gomuku.AssignRoomReply);
+				GetProxy<GomukuProxy>().ConnectToBattleServer(reply.address, reply.roomId);
+			});
+		}
+
+		public void ConnectToGameServer(string address)
+		{
+			NetworkManager.instance.Get(NetType.Hall).onConnected = (obj) =>
+			{
+
+			};
+
+			NetworkManager.instance.Connect(NetType.Hall, address);
+		}
+
+		public void Login(string uuid)
+		{
+			LoginRequest msg = new LoginRequest();
+			msg.deviceUID = uuid;
+			network.SendMessage<LoginRequest, LoginReply>
+			(msg, (reply) =>
+			{
+				m_mainPlayerData.SetData(reply);
+			});
+		}
+	}
+}
---
+++ GameServer/MainServer/MainServer/Tools/Debug.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Debug
+{
+    public static void Log(string str)
+    {
+        Console.WriteLine(str);
+    }
+    public static void LogError(string str)
+    {
+        Console.WriteLine(str);
+    }
+}
